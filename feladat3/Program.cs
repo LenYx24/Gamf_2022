6697b6a99fe83d751e714fcf3e576c59b8f34991
LenYx24 <65910507+LenYx24@users.noreply.github.com>
2022-11-16 19:38:07 +0100

    Code cleanup, adding numberPyramid class

--- feladat3/Program.cs
+++ feladat3/Program.cs
@@ -15,47 +15,90 @@ namespace feladat3
                 Nums = s;
             }
         }
+        class NumberPyramid
+        {
+            List<Row> rows;
+            public void SetFirstRowFromFile()
+            {
+                string[] text = File.ReadAllLines("szamok7.txt");
+                List<int> nums = text.ToList().ConvertAll(x => Convert.ToInt32(x));
+                Row firstRow = new(nums);
+                rows.Add(firstRow);
+            }
+            public void calculateRows()
+            {
+                if (rows.Count == 0) return;
+                while (rows[^1].Nums.Count >= 2)
+                {
+                    List<int> newNums = new();
+                    for (int i = 1; i < rows[^1].Nums.Count; i++)
+                    {
+                        newNums.Add(rows[^1].Nums[i] + rows[^1].Nums[i - 1]);
+                    }
+                    rows.Add(new(newNums));
+                }
+            }
+            public void PrintRowsInReverse()
+            {
+                for (int i = rows.Count - 1; i >= 0; i--)
+                {
+                    Console.WriteLine($"{i + 1}: {String.Join(" ", rows[i].Nums)}");
+                }
+            }
+            public void PrintRows()
+            {
+                for (int i = 0; i < rows.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}: {String.Join(" ", rows[i].Nums)}");
+                }
+            }
+            public int GetSumOfRowValues(int index)
+            {
+                return rows[index].Nums.Sum();
+            }
+            public List<Row> Rows { get; }
+            public int LastRowsNumber { get { return rows[^1].Nums[0]; } } // Legfelső (utolsó) sor száma
+        }
         static void Main(string[] args)
         {
-            List<Row> rows = new();
-            rows.Add(new(File.ReadAllLines("szamok7.txt").ToList().ConvertAll(x => Convert.ToInt32(x))));
-            while (rows[^1].Nums.Count >= 2)
-            {
-                List<int> newNums = new();
-                for (int i = 1; i < rows[^1].Nums.Count; i++)
-                {
-                    newNums.Add(rows[^1].Nums[i] + rows[^1].Nums[i - 1]);
-                }
-                rows.Add(new(newNums));
-            }
-            for (int i = rows.Count - 1; i >= 0; i--)
-            {
-                Console.WriteLine($"{i + 1}: {String.Join(" ", rows[i].Nums)}");
-            }
-            Row row6 = rows[5];
+            // A piramis elkészítése
+            NumberPyramid numberPyramid = new NumberPyramid();
+            numberPyramid.SetFirstRowFromFile();
+            numberPyramid.calculateRows();
+            numberPyramid.PrintRows();
+
+            // a rész
+            Row row6 = numberPyramid.Rows[5];
             int not3digitNums = 0;
             foreach (int x in row6.Nums)
             {
                 if (x < 100 || x > 999) not3digitNums++;
             }
+            //LINQ:
+            int n = row6.Nums.Where(x => x < 100 || x > 999).Count();
             Console.WriteLine($"a) A számpiramos 6. sorában {not3digitNums} nem háromjegyű szám található");
-            Console.WriteLine($"b) A számpiramos legfelső sorában a {rows[^1].Nums[0]} szám szerepel");
 
+            // b rész
+            string b = numberPyramid.LastRowsNumber.ToString();
+            Console.WriteLine($"b) A számpiramos legfelső sorában a {b} szám szerepel");
+
+            // c rész
+            numberPyramid.PrintRowsInReverse();
             List<double> quotients = new();
-            for (int i = 0; i < rows.Count; i++)
+            for (int i = 1; i < numberPyramid.Rows.Count; i++)
             {
-                Console.WriteLine($"{String.Join(" ", rows[i].Nums)}");
+                double rowSums = Convert.ToDouble(numberPyramid.GetSumOfRowValues(i));
+                double aboveRowSums = numberPyramid.GetSumOfRowValues(i-1);
+                double quotient = rowSums / aboveRowSums;
+                quotients.Add(quotient);
             }
-            int counter = 0;
-            for (int i = 1; i < rows.Count; i++)
-            {
-                double num = Convert.ToDouble(rows[i].Nums.Sum()) / rows[i - 1].Nums.Sum();
-                quotients.Add(num);
-                if (num > 1.5) counter++;
-            }
-            Console.WriteLine("HÁNYADOSOK\n--------------");
-            quotients.ForEach(x => Console.WriteLine(x));
-            Console.WriteLine($"c) {counter} sor esetén lesz a hányados nagyobb 1,5-nél");
+            
+            int c = quotients.Where(x => x > 1.5).Count();// Megszámolja hogy hány olyan hányados van amely értéke nagyobb 1,5nél
+            Console.WriteLine($"c) {c} sor esetén lesz a hányados nagyobb 1,5-nél");
+            // TESZT
+            // Hányadosok kiírása
+            //Console.WriteLine("HÁNYADOSOK\n--------------");
+            //quotients.ForEach(x => Console.WriteLine(x));
         }
     }
 }

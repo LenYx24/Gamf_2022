a7518ee3898e8e26721bd5966701101e64f3b869
RedBird <35497481+redbirdjs@users.noreply.github.com>
2022-11-17 17:14:55 +0100

    Made changes in Program.cs

--- feladat1/Program.cs
+++ feladat1/Program.cs
@@ -52,7 +52,7 @@ namespace feladat1
         }
 
         static bool isPrime(int num) {
-          for (int i = 2; i < num; i++) {
+          for (int i = 2; i < num / 2; i++) {
             if (num % i == 0) return false;
           }
           return true;

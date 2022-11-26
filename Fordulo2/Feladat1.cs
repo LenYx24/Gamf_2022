b682fe6edea15a8424c4a4617da9373b59b62910
LenYx24 <65910507+LenYx24@users.noreply.github.com>
2022-11-26 20:08:14 +0100

    weragdf

--- Fordulo2/Feladat1.cs
+++ Fordulo2/Feladat1.cs
@@ -63,12 +63,19 @@ namespace Fordulo2
             return db;
         }
 
-        static bool isPrime(int num) {
-          if (num <= 1) return false;
-          for (int i = 2; i <= num / 2; i++) {
-            if (num % i == 0) return false;
-          }
-          return true;
+        static bool isPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
         }
         static bool isOneDigitPrime(int num)
         {

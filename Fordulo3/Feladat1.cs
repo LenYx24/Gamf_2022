127dd910df6f609617cfb890e6a550440c983758
RedBird <35497481+redbirdjs@users.noreply.github.com>
2022-11-29 22:38:59 +0100

    Created Fordulo3

---
+++ Fordulo3/Feladat1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Fordulo3
+{
+    internal class Feladat1 {
+        public static void Start() {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
---
+++ Fordulo3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Fordulo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Feladat1.Start();
+            Feladat2.Start();
+            Feladat3.Start();
+        }
+    }
+}
